ef7209a189eb29af23b3c024459e4318cce6c92e
ManoellaR <manu.rockemba@gmail.com>
2022-03-04 17:03:23 -0300

    configurei a interrupção e wdt

--- software/TrabalhoFinal_R&M.X/main.c
+++ software/TrabalhoFinal_R&M.X/main.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
     UFSC- Universidade Federal de Santa Catarina
     *Project Name:
- Trabalho Final - Irrigaï¿½ï¿½o
+ Trabalho Final - Irrigação
     *Copyright:;
             Rodrigo Ferraz Souza
             Manoella Rockembach
@@ -21,6 +21,12 @@
 
 #define _XTAL_FREQ 4000000
 
+//configurando o watchdog time
+//#pragma config WDTE = ON //habilita o uso do WDT
+//#pragma config FOSC = HS //define uso do clock externo em 4 ou 20Mhz
+//#pragma config PWRTE = ON //habilita reset ao ligar (pode ser usado no lugar do capacitor)
+//#pragma config BOREN = ON //Habilita o reset por Brown-out (vales na tensão)
+
 #define SENSOR_UMIDADE      PORTAbits.RA0
 
 #define BTN_IRRIGAR         PORTBbits.RB0
@@ -32,20 +38,27 @@
 #define LED_IRRIGACAO       PORTBbits.RB6
 #define LED_UMIDADE         PORTBbits.RB7
 
-#define EN                  PORTDbits.RD0
-#define RS                  PORTDbits.RD1
-#define RW                  PORTDbits.RD2
-#define D4                  PORTDbits.RD4
-#define D5                  PORTDbits.RD5
-#define D6                  PORTDbits.RD6
-#define D7                  PORTDbits.RD7
+//#define EN                  PORTDbits.RD0
+//#define RS                  PORTDbits.RD1
+//#define RW                  PORTDbits.RD2
+//#define D4                  PORTDbits.RD4
+//#define D5                  PORTDbits.RD5
+//#define D6                  PORTDbits.RD6
+//#define D7                  PORTDbits.RD7
 
+//*** define pinos referentes a interface com LCD
+#define RS RD2
+#define EN RD3
+#define D4 RD4
+#define D5 RD5
+#define D6 RD6
+#define D7 RD7
 
 // Adiciona a lib do LCD
 #include "lcd.h"
 
-// mili_s ï¿½ o tempo desejado pra esperar
-// ï¿½ necessï¿½rio essa funï¿½ï¿½o pois o tempo serï¿½ variado, dependendo do setup
+// mili_s e o tempo desejado pra esperar
+// e necessario essa funcao pois o tempo sera variado, dependendo do setup
 void setupTimer(int mili_s)
 {
     return;
@@ -67,7 +80,11 @@ void handleTimerInterruption()
 
 void handleExternalInterruption()
 {
-
+    if(INTF){
+        //if(!irrigacao_ativa)
+        //irrigar
+        LED_IRRIGACAO = 1;
+    }
     return;
 }
 
@@ -80,6 +97,9 @@ void __interrupt() interrupcao(void)
 
 void setupExternalInterruption()
 {
+    OPTION_REGbits.INTEDG = 0;
+	INTCONbits.GIE = 1;
+	INTCONbits.INTE = 1;
     return;
 }
 
@@ -102,6 +122,11 @@ void verifySensor()
 
 void setupWatchdogTimer()
 {
+    //OPTION_REGbits.PSA = 1; //define que o prescaler esta associado ao WTD
+    //OPTION_REGbits.PS0 = 0; // define o prescaler do WTD para 1:64 (1152ms)
+    //OPTION_REGbits.PS1 = 1;
+    //OPTION_REGbits.PS2 = 1;
+    //CLRWDT();
     return;
 }
 
@@ -115,13 +140,24 @@ void main(void)
     
     setupExternalInterruption();
     setupWatchdogTimer();
-    setupTimer(500); // sï¿½ p ter um valor default
+    setupTimer(500); // so p ter um valor default
     Lcd_Init();
+    
+    PORTB = 0;
 
     while (1)
     {
         verifySensor();
         verifyMenu();
+        Lcd_Clear();                    //limpa LCD
+        Lcd_Set_Cursor(1,1);            //Poe cursor linha 1 coluna 1
+    
+        Lcd_Write_String("OLA MUNDO");  //escreve string
+        __delay_ms(1000);
+    
+        Lcd_Set_Cursor(2,1);             //linha 2 coluna 1
+        Lcd_Write_String("MUNDO DOIDO"); //escreve string]
+        __delay_ms(2000);
     }
     return;
 }
